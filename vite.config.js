c5381a02bea03853e2abba1881ac7561250d2219
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-10-04 17:57:23 +0300

    Breeze install

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,11 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
-        {
-            name: 'blade',
-            handleHotUpdate({ file, server }) {
-                if (file.endsWith('.blade.php')) {
-                    server.ws.send({
-                        type: 'full-reload',
-                        path: '*',
-                    });
-                }
-            },
-        }
     ],
 });
