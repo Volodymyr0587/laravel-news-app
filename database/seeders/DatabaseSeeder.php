de852c33bd02f4091786c179cfd6137cf8bd3c2f
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-10-06 17:40:28 +0300

    DB seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Article;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,12 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Tag::create(['name' => 'Laravel PHP VueJS', 'slug' => 'laravel']);
+        Article::create(['title' => 'First News', 'photo' => 'path/to/photo', 'content' => 'News Content ...', 'active' => 1, 'tags' => 'tag1 tag2']);
+        Article::create(['title' => 'Second News', 'photo' => 'path/to/photo', 'content' => 'News Content ...', 'active' => 0, 'tags' => 'tag2 tag3']);
+        Article::create(['title' => 'Third News', 'photo' => 'path/to/photo', 'content' => 'News Content ...', 'active' => 1, 'tags' => 'tag3 tag4']);
+        Article::create(['title' => 'Fourth News', 'photo' => 'path/to/photo', 'content' => 'News Content ...', 'active' => 0, 'tags' => 'tag2 tag1']);
+
     }
 }
