6f9cd06d6428cc4c27812976a465af1d82814f1c
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-10-07 13:18:13 +0300

    Message for admin and regular user at dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,69 +7,17 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 overflow-hidden shadow-sm sm:rounded-lg">
-                {{-- <div class="p-6 text-gray-900 dark:text-gray-100"> --}}
-
-                    {{-- @if (Auth::user()->is_admin) --}}
-                    @admin
-
-                    <form method="POST" action="" enctype="multipart/form-data"
-                        class="p-4 bg-white dark:bg-slate-800 rounded-md">
-                        @csrf
-                        <div class="grid gap-6 mb-6 md:grid-cols-2">
-                            <div>
-                                <label for="title"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
-                                <input type="text" id="title" name="title"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="News title" value="{{ old('title', request()->title) }}">
-                                @error('title')
-                                    <div class="text-sm text-red-400">{{ $message }}</div>
-                                @enderror
-                            </div>
-                            <div>
-                                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                                    for="photo">Upload image</label>
-                                <input
-                                    class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                                    id="photo" type="file" name="photo">
-                                @error('photo')
-                                    <div class="text-sm text-red-400">{{ $message }}</div>
-                                @enderror
-                            </div>
-                            <div>
-                                <label for="content"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Content</label>
-                                <textarea id="content" name="content" rows="4"
-                                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="Write content here...">{{ old('content', request()->content) }}</textarea>
-                                @error('content')
-                                    <div class="text-sm text-red-400">{{ $message }}</div>
-                                @enderror
-                            </div>
-                            <div>
-                                <label for="tags"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tags [words separated by spaces]</label>
-                                <input type="text" id="tags" name="tags"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="Tags" value="{{ old('tags', request()->tags) }}">
-                                @error('tags')
-                                    <div class="text-sm text-red-400">{{ $message }}</div>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div>
-                            <button type="submit"
-                                class=" bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 mt-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Create</button>
-                        </div>
-                    </form>
-                    @else
+            <div
+                class="bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 overflow-hidden shadow-sm sm:rounded-lg">
+                @admin
                     <div class="p-6">
-                        {{ __("You're logged in!") }}
+                        {{ __("You're logged in as Administrator! Click 'Articles' link to manage content.") }}
                     </div>
-                    @endadmin
-                {{-- </div> --}}
+                @else
+                    <div class="p-6">
+                        {{ __("You're logged in as guest user. Click logo link to see our content.") }}
+                    </div>
+                @endadmin
             </div>
         </div>
     </div>
