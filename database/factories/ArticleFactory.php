88d8698ef7e25692f0413ce4618270a5ca58cafb
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-10-09 17:36:18 +0300

    Factories; seeders

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->title,
+            'photo' => '/public/photo.jpg',
+            'content' => $this->faker->text(300),
+            'active' => rand(0, 1),
+        ];
+    }
+}
